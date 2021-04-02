5bccb555da3d05f5dbda380835194f82fe2972e6
Genki Sato <genki.sato@lasca.co.jp>
2021-04-02 20:00:27 +0900

    jsx support

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,16 @@ const ENDPOINT_URL =
 
 export default async function loader(source: string) {
   const ext = this.resourcePath.split("/").reverse()[0].split(".")[1];
-  console.log(this.request);
-  console.log(this.query);
-  console.log(this.resource);
-  console.log(this.resourceQuery);
-  console.log(this.resourcePath);
-  console.log(ext);
 
   const res = await axios.get(`${ENDPOINT_URL}/page/hoge`);
 
-  if (this.resourceQuery.includes("type=template")) {
-    console.log("template");
+  if (ext === "jsx") {
+    source = source.replace(/\<lasca\>\<\/lasca\>/g, res.data.jsx);
+  } else if (this.resourceQuery.includes("type=template")) {
     source = source.replace(/\<lasca\>\<\/lasca\>/g, res.data.template);
   } else if (this.resourceQuery.includes("type=style")) {
-    console.log("style");
     source = source + res.data.css;
   }
 
-  console.log(source);
   return source;
 }
