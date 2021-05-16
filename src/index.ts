9020c3b2013615329a10fbd48feb86325e0538bf
Genki Sato <genki.sato@lasca.co.jp>
2021-05-16 21:14:42 +0900

    graphQL request

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,37 @@
-import axios from "axios";
+import { GraphQLClient, gql } from "graphql-request";
 
-const ENDPOINT_URL =
-  "https://zjhb2e1fl7.execute-api.ap-northeast-1.amazonaws.com/dev";
+const ENDPOINT_URL = "https://api.lasca.app/query";
 
 export default async function loader(source: string) {
-  const ext = this.resourcePath.split("/").reverse()[0].split(".")[1];
+  const endpoint = process.env.ENDPOINT_URL || ENDPOINT_URL;
+  // const ext = this.resourcePath.split("/").reverse()[0].split(".")[1];
 
-  const res = await axios.get(`${ENDPOINT_URL}/page/hoge`);
+  const client = new GraphQLClient(endpoint);
+  client.setHeader("X-LASCA-TOKEN", process.env.LASCA_API_TOKEN || "no_token");
 
-  if (ext === "jsx") {
-    source = source.replace(
-      /\<lasca\>\<\/lasca\>/g,
-      `<div>${res.data.jsx}<style>{\`${res.data.css}\`}</style></div>`
-    );
-  } else if (this.resourceQuery.includes("type=template")) {
-    source = source.replace(/\<lasca\>\<\/lasca\>/g, res.data.template);
-  } else if (this.resourceQuery.includes("type=style")) {
-    source = source + res.data.css;
-  }
+  const query = gql`
+    query GetBreakpoints {
+      breakpoints {
+        min
+        max
+        figma
+      }
+    }
+  `;
+
+  const res = await client.request(endpoint, query);
+  console.log(res);
+
+  // if (ext === "jsx") {
+  //   source = source.replace(
+  //     /\<lasca\>\<\/lasca\>/g,
+  //     `<div>${res.data.jsx}<style>{\`${res.data.css}\`}</style></div>`
+  //   );
+  // } else if (this.resourceQuery.includes("type=template")) {
+  //   source = source.replace(/\<lasca\>\<\/lasca\>/g, res.data.template);
+  // } else if (this.resourceQuery.includes("type=style")) {
+  //   source = source + res.data.css;
+  // }
 
   return source;
 }
--- test/loader.spec.ts
+++ test/loader.spec.ts
@@ -1,16 +1,6 @@
-import axios from "axios";
 import { getOutput } from "./helper";
 
-jest.mock("axios");
-
 test("simple vue.js", async () => {
-  const resp = {
-    data: {
-      template: "<p>hoge</p>",
-    },
-  };
-  (axios as jest.Mocked<typeof axios>).get.mockResolvedValue(resp);
-
   const output = await getOutput("sample.vue");
   expect(output).toBe('"<template><p>hoge</p></template>"');
 });
