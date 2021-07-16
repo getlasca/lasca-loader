4ce1b6aa17903cd9afffdafbd57426e8803ee149
Genki Sato <genki.sato@lasca.co.jp>
2021-07-16 17:40:02 +0900

    fix loader test

--- test/fixtures/sample.vue
+++ test/fixtures/sample.vue
@@ -1,3 +1,24 @@
-<template>
+<template lang="lasca">
   <lasca></lasca>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 10,
+    };
+  },
+  methods: {
+    click: function ($event) {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style>
+.dummy {
+  color: red;
+}
+</style>
