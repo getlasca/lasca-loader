93233ce8ed7bfac0e8fcec4cb025641686ed7c91
Genki Sato <genki.sato@lasca.co.jp>
2021-10-17 22:16:03 +0900

    Revert "update directory structure"
    This reverts commit 9ed2d810b7de12b5b220603420996e8cb40b62ae.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import fs from "fs";
 import { getOptions } from "loader-utils";
-import { convertVueTemplate, convertVueCss } from "./vue/converter";
+import { JSDOM } from "jsdom";
 import { Component, FileComponent } from "./types";
 
 export default function loader(source: string) {
@@ -33,4 +33,56 @@ export default function loader(source: string) {
   return source;
 }
 
-export { LascaLoaderVuePlugin } from "./vue/plugin";
+function convertVueTemplate(
+  source: string,
+  components: Component[],
+  file: string
+): string {
+  const doc = new JSDOM(source).window.document;
+  const lascaTags = doc.getElementsByTagName("lasca");
+
+  for (let i = 0; i < lascaTags.length; i++) {
+    const attr = lascaTags[i].attributes.getNamedItem("component");
+    if (!attr) {
+      throw new Error(
+        `component attribute of lasca tag is not set at ${file}.`
+      );
+    }
+    const component = components.find(
+      (component) => component.name === attr.value
+    );
+    if (!component) {
+      throw new Error(`component name ${attr.value} is not pulled at ${file}.`);
+    }
+    lascaTags[i].outerHTML = component.template;
+  }
+
+  return doc.body.innerHTML;
+}
+
+function convertVueCss(
+  source: string,
+  file: string,
+  components: Component[],
+  fileComponents: FileComponent[]
+): string {
+  let output = source;
+
+  const fileComponent = fileComponents.find(
+    (fileComponent) => fileComponent.file === file
+  );
+  if (!fileComponent) {
+    return output;
+  }
+
+  const targetComponents = components.filter((component) => {
+    return fileComponent.components.includes(component.name);
+  });
+  for (let i = 0; i < targetComponents.length; i++) {
+    output = output + targetComponents[i].css;
+  }
+
+  return output;
+}
+
+export { LascaLoaderVuePlugin } from "./plugin";
